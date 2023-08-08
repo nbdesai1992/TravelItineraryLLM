e7b4878a150ae4ba5eb604696896d0390b8dc9b7
Neal Desai <nealdesai@MacBook-Pro.lan>
2023-08-07 23:16:22 -0700

    Additional UI improvements

--- app/main.py
+++ app/main.py
@@ -1,16 +1,19 @@
+import re
+
 from flask import Flask, request, render_template
 from models.user import User
-from models.itinerary import Itinerary
 from services.openai_service import OpenAIService
 
 app = Flask(__name__)
 
 @app.route('/', methods=['GET', 'POST'])
 def index():
-    itinerary = None
+    itinerary = {}
+    user = None  # Initialize user here
+
     if request.method == 'POST':
         location = request.form.get('location')
-        days = request.form.get('days')
+        days = int(request.form.get('days'))
         budget = request.form.get('budget')
         activities = request.form.get('activities')
         cuisine = request.form.get('cuisine')
@@ -18,14 +21,41 @@ def index():
         user = User(location=location, days=days, budget=budget, activities=activities, cuisine=cuisine)
 
         service = OpenAIService()
-        itinerary_details = service.generate_itinerary(user.preferences)
+        itinerary_string = service.generate_itinerary(user.preferences)
+        #print(itinerary_string)
 
-        if itinerary_details:
-            itinerary = itinerary_details
+        if itinerary_string:
+            days_splits = re.split(r'(Day \d+)', itinerary_string)
+            for i in range(1, len(days_splits), 2): # Skipping over the non-"Day x" pieces
+                day = days_splits[i].strip()
+                content = days_splits[i + 1].strip().lstrip(": - ")
+                activities = {}
+
+                morning = ""
+                afternoon = ""
+
+                if "Morning:" in content:
+                    morning, rest = content.split("Morning:", 1)
+                    morning = morning.strip().lstrip("- ").rstrip("-")
+                    if "Afternoon:" in rest:
+                        morning_activities, afternoon = rest.split("Afternoon:", 1)
+                        morning += " " + morning_activities.strip().lstrip("- ").rstrip("-")
+                        afternoon = afternoon.strip().lstrip("- ").rstrip("-")
+                    else:
+                        afternoon = ""
+
+                if "Afternoon:" in content and not morning:
+                    afternoon, rest = content.split("Afternoon:", 1)
+                    afternoon = afternoon.strip().lstrip("- ").rstrip("-")
+
+                activities["morning"] = morning
+                activities["afternoon"] = afternoon
+
+                itinerary[day] = activities
         else:
             return "Error: Could not generate itinerary. Please check your input and try again."
 
-    return render_template('index.html', itinerary=itinerary)
+    return render_template('index.html', itinerary=itinerary, user=user)
 
 if __name__ == "__main__":
     app.run(debug=True)
--- app/services/openai_service.py
+++ app/services/openai_service.py
@@ -13,34 +13,30 @@ class OpenAIService:
 
     key = KeyManager()
     openai.api_key = key.get_api_key('NBDKey')
-    
+
     @staticmethod
     def generate_itinerary(user_preferences):
+        # Define the instruction for the model to generate a structured response
+        instruction = f"Based on the user preferences: {user_preferences}, please provide a travel itinerary for each day in the following format: "
+        example_format = "\"Day 1: - Morning: [Morning activities here]. - Afternoon: [Afternoon activities here].\""
+
+        # Merge the instruction and example_format
+        complete_prompt = instruction + example_format
+
         messages = [
             {"role": "system", "content": "You are a helpful assistant."},
-            {"role": "user", "content": f"I need a travel itinerary. Here are my preferences: {user_preferences}"}
+            {"role": "user", "content": complete_prompt}
         ]
         
         response = openai.ChatCompletion.create(
-          model="gpt-3.5-turbo",
-          messages=messages,
+        model="gpt-3.5-turbo",
+        messages=messages,
         )
 
         details = response["choices"][0]["message"]["content"]
 
-        # The response is assumed to be in JSON format. Parse it into a dictionary
-        # try:
-        #     details = json.loads(response['choices'][0]['message']['content'])
-        # except json.JSONDecodeError:
-        #     print("Error: The response from the OpenAI API is not in valid JSON format.")
-        #     return None
-
-        # Validate the structure of the dictionary
-        # if not OpenAIService._validate_itinerary(details):
-        #     print("Error: The structure of the itinerary is invalid.")
-        #     return None
-
         return details
+
 
     @staticmethod
     def _validate_itinerary(details):
