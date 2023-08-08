c0b804a312abdd6b916feb826cf9faf1015a6f4d
Neal Desai <nealdesai@MacBook-Pro.lan>
2023-08-07 20:44:10 -0700

    UI improvements

--- app/main.py
+++ app/main.py
@@ -5,29 +5,27 @@ from services.openai_service import OpenAIService
 
 app = Flask(__name__)
 
-@app.route('/', methods=['GET'])
+@app.route('/', methods=['GET', 'POST'])
 def index():
-    return render_template('index.html')
+    itinerary = None
+    if request.method == 'POST':
+        location = request.form.get('location')
+        days = request.form.get('days')
+        budget = request.form.get('budget')
+        activities = request.form.get('activities')
+        cuisine = request.form.get('cuisine')
 
-@app.route('/generate', methods=['POST'])
-def generate_itinerary():
-    location = request.form.get('location')
-    days = request.form.get('days')
-    budget = request.form.get('budget')
-    activities = request.form.get('activities')
-    cuisine = request.form.get('cuisine')
+        user = User(location=location, days=days, budget=budget, activities=activities, cuisine=cuisine)
 
-    user = User(location=location, days=days, budget=budget, activities=activities, cuisine=cuisine)
+        service = OpenAIService()
+        itinerary_details = service.generate_itinerary(user.preferences)
 
-    service = OpenAIService()
-    itinerary_details = service.generate_itinerary(user.preferences)
+        if itinerary_details:
+            itinerary = itinerary_details
+        else:
+            return "Error: Could not generate itinerary. Please check your input and try again."
 
-    if itinerary_details is None:
-        return "Error: Could not generate itinerary. Please check your input and try again."
-
-    itinerary = itinerary_details
     return render_template('index.html', itinerary=itinerary)
 
 if __name__ == "__main__":
     app.run(debug=True)
-
